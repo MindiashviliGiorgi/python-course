233d51c90b92aaf57dbaf2b8a01ade377f58ad13
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2024-01-22 23:52:19 +0400

    python first steps

---
+++ app.py
@@ -0,0 +1,43 @@
+# 1 Drawing a Shape
+
+# print('    /|')
+# print('   / |')
+# print('  /  |')
+# print(' /   |')
+# print('/____|')
+
+# 2 Variables & Data types
+
+# character_name = "Giorgi"
+# character_age = 23
+
+# print("There once was a man name" + character_name)
+# print("he was " + str(character_age) + " years old")
+
+# 3 Working with strings
+
+# pharse = "Giraffe Academy"
+# print(pharse.isupper()) # returning boolean is upper or not - true/false
+# print(pharse.upper())
+# print(pharse.lower())
+# print(len(pharse)) # returning string length
+# print(pharse.replace("Giraffe", "Giorgi's")) # will replace giraffe with Giorgi's
+
+# 4 Working with numbers
+
+# num = -5
+
+# print(abs(num)) # convert negative number to normal number
+# print(str(num) + " is string") # converting number to string
+# print(pow(2, 3)) # 2 * 2 * 2
+# print(min(2, 3, 5, 19, 1)) # finding minimum number
+# print(round(3.3)) # returning whole number 3.3 = 3 / 3.5 = 4
+
+# from math import *
+
+# need 'math' for use floor and ceil function
+# print(floor(3.6)) # returning 3
+# print(ceil(3.1)) # returning highest 4
+# print (sqrt(36)) # returning 6.0,
+
+# python app.py
